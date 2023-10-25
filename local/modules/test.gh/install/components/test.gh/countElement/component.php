8d0d5f66bb09d5b2bf8057868a6771b7dbae2b6e
arizel <chelpuck0@gmail.com>
2023-10-26 02:12:57 +0300

    delete old component

--- local/modules/test.gh/install/components/test.gh/countElement/component.php
+++
@@ -1,8 +0,0 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-CModule::IncludeModule('test.gh');
-
-
-$arResult = \Gh\General::getCount($arParams['SITE']);
-$this->IncludeComponentTemplate();
-?> 
